896806df483af6f8a5d87d03e1cdce629768a00b
Shekoo110 <alshekh989@gmail.com>
2026-08-05 01:53:24 +0300

    Remove DNS and dotenv setup from index.js
    Removed DNS and dotenv configuration from index.js

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const dns = require('dns')
-dns.setServers(['8.8.8.8', '1.1.1.1'])
-
-const dotenv = require('dotenv')
-dotenv.config()
-
 const fs = require('fs')
 const battleLocks = new Set();
 const bankSystem = require("./bankSystem")
